cf7b5174f605b78cd5df4c0bcbe87c4983d53946
Nightlights524 <Nightlights524@users.noreply.github.com>
2020-12-28 16:16:28 -0600

    1st test complete

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,216 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite('Functional Tests', () => {
+  test('Create an issue with every field', done => {
+    chai.request(server)
+        .post('/api/issues/apitest')
+        .query({
+          issue_title: 'Title',
+          issue_text: 'Issue Text',
+          created_by: 'Created By',
+          assigned_to: 'Assigned To',
+          status_text: 'Status Text'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          done();
+        });
+  });
+  test('Create an issue with every field', done => {
+    chai.request(server)
+        .post('/api/issues/apitest')
+        .query({
+          issue_title: 'Title',
+          issue_text: 'Issue Text',
+          created_by: 'Created By',
+          assigned_to: 'Assigned To',
+          status_text: 'Status Text'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          done();
+        });
+  });
+  test('Create an issue with every field', done => {
+    chai.request(server)
+        .post('/api/issues/apitest')
+        .query({
+          issue_title: 'Title',
+          issue_text: 'Issue Text',
+          created_by: 'Created By',
+          assigned_to: 'Assigned To',
+          status_text: 'Status Text'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          done();
+        });
+  });
+  test('Create an issue with every field', done => {
+    chai.request(server)
+        .post('/api/issues/apitest')
+        .query({
+          issue_title: 'Title',
+          issue_text: 'Issue Text',
+          created_by: 'Created By',
+          assigned_to: 'Assigned To',
+          status_text: 'Status Text'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          done();
+        });
+  });
+  test('Create an issue with every field', done => {
+    chai.request(server)
+        .post('/api/issues/apitest')
+        .query({
+          issue_title: 'Title',
+          issue_text: 'Issue Text',
+          created_by: 'Created By',
+          assigned_to: 'Assigned To',
+          status_text: 'Status Text'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          done();
+        });
+  });
+  test('Create an issue with every field', done => {
+    chai.request(server)
+        .post('/api/issues/apitest')
+        .query({
+          issue_title: 'Title',
+          issue_text: 'Issue Text',
+          created_by: 'Created By',
+          assigned_to: 'Assigned To',
+          status_text: 'Status Text'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          done();
+        });
+  });
+  test('Create an issue with every field', done => {
+    chai.request(server)
+        .post('/api/issues/apitest')
+        .query({
+          issue_title: 'Title',
+          issue_text: 'Issue Text',
+          created_by: 'Created By',
+          assigned_to: 'Assigned To',
+          status_text: 'Status Text'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          done();
+        });
+  });
+  test('Create an issue with every field', done => {
+    chai.request(server)
+        .post('/api/issues/apitest')
+        .query({
+          issue_title: 'Title',
+          issue_text: 'Issue Text',
+          created_by: 'Created By',
+          assigned_to: 'Assigned To',
+          status_text: 'Status Text'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          done();
+        });
+  });
+  test('Create an issue with every field', done => {
+    chai.request(server)
+        .post('/api/issues/apitest')
+        .query({
+          issue_title: 'Title',
+          issue_text: 'Issue Text',
+          created_by: 'Created By',
+          assigned_to: 'Assigned To',
+          status_text: 'Status Text'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          done();
+        });
+  });
+  test('Create an issue with every field', done => {
+    chai.request(server)
+        .post('/api/issues/apitest')
+        .query({
+          issue_title: 'Title',
+          issue_text: 'Issue Text',
+          created_by: 'Created By',
+          assigned_to: 'Assigned To',
+          status_text: 'Status Text'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          done();
+        });
+  });
+  test('Create an issue with every field', done => {
+    chai.request(server)
+        .post('/api/issues/apitest')
+        .query({
+          issue_title: 'Title',
+          issue_text: 'Issue Text',
+          created_by: 'Created By',
+          assigned_to: 'Assigned To',
+          status_text: 'Status Text'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          done();
+        });
+  });
+  test('Create an issue with every field', done => {
+    chai.request(server)
+        .post('/api/issues/apitest')
+        .query({
+          issue_title: 'Title',
+          issue_text: 'Issue Text',
+          created_by: 'Created By',
+          assigned_to: 'Assigned To',
+          status_text: 'Status Text'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          done();
+        });
+  });
+  test('Create an issue with every field', done => {
+    chai.request(server)
+        .post('/api/issues/apitest')
+        .query({
+          issue_title: 'Title',
+          issue_text: 'Issue Text',
+          created_by: 'Created By',
+          assigned_to: 'Assigned To',
+          status_text: 'Status Text'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          done();
+        });
+  });
+  test('Create an issue with every field', done => {
+    chai.request(server)
+        .post('/api/issues/apitest')
+        .query({
+          issue_title: 'Title',
+          issue_text: 'Issue Text',
+          created_by: 'Created By',
+          assigned_to: 'Assigned To',
+          status_text: 'Status Text'
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          done();
+        });
+  });
   
 });
