39a53f5d9947992fecaaf4e04f7f25ec244f44bc
Nightlights524 <Nightlights524@users.noreply.github.com>
2020-12-27 23:03:59 -0600

    Begin PUT request

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,11 @@
 'use strict';
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -32,17 +36,17 @@ module.exports = async function (app) {
 
       const documents = await Issue.find(queryObject).exec();
       // const documents = await Issue.find({project: project}).orFail().exec();
-      res.json(documents);
+      return res.json(documents);
     }
     catch (error) {
       console.error(error.message);
-      res.send(error.message);
+      return res.send(error.message);
     }
   })
   
   .post(async function (req, res){
     try {
-      // Check to make sure request has required fields
+      // First check to make sure the request has all required fields
       if (!Object.prototype.hasOwnProperty.call(req.body, 'issue_title') ||
           !Object.prototype.hasOwnProperty.call(req.body, 'issue_text') ||
           !Object.prototype.hasOwnProperty.call(req.body, 'created_by'))
@@ -57,13 +61,6 @@ module.exports = async function (app) {
       const created_by = req.body.created_by || "";
       const assigned_to = req.body.assigned_to || "";
       const status_text = req.body.status_text || "";
-      
-      // if (issue_title === "" ||
-      //     issue_text === "" ||
-      //     created_by === "") 
-      // {
-      //   return res.json({ error: 'required field(s) missing' });
-      // }
 
       const newIssue = await Issue.create({
         project: project,
@@ -87,18 +84,36 @@ module.exports = async function (app) {
   
   .put(async function (req, res){
     try {
-      let project = req.params.project;
+      console.log(req.body);
+
+      // First check to make sure the request has the required '_id' field
+      if (!Object.prototype.hasOwnProperty.call(req.body, '_id'))
+      {
+        return res.json({error: 'missing _id'});
+      }
+
+      // // Then make sure request has other update fields
+      // if (request doesn't have update fields))
+      // {
+      //   return res.json({error: 'no update field(s) sent', _id: req.body._id });
+      // }
+
+      // const project = req.params.project;
+
+      const updatedDoc = await Issue.findByIdAndUpdate(req.body._id, req.body, {new: true}).exec();
+
+      console.log(updatedDoc);
+
+      return res.json({result: 'successfully updated', _id: updatedDoc._id})
     }
     catch (error) {
       console.error(error.message);
-      res.send(error.message);
+      return res.json({error: 'could not update', _id: req.body._id});
     }
   })
   
   .delete(async function (req, res){
     try {
-      // let project = req.params.project;
-
       if (!req.body._id) {
         return res.json({error: 'missing _id' });
       }
